dd3074482642e1f724269f02d5979c96510e62d6
7h1b0 <wyrm1992@hotmail.fr>
2014-09-04 01:06:12 +0200

    Optimize Code

--- controllers/activity.js
+++ controllers/activity.js
@@ -2,15 +2,15 @@ var process = require('./process')
 	, config = require('../config/config.json');
 
 exports.init = function(app){
-	app.route('/activity').get(function(req,res){
+	app.route('/activity').get(function(req,res,next){
+		
 		if(req.param('token') == config.tokenActivity){
 			var script = req.param('name') + '.sh';
 			process.exec(script);
 			res.render('ok',{script : script});	
 			
 		}else{
-			console.log(new Date() + 'Fail token');
-			res.status(404).render('404.jade');
+			next();
 		}
 	});
 }
--- controllers/power.js
+++ controllers/power.js
@@ -2,16 +2,15 @@ var process = require('./process')
 	, config = require('../config/config.json');
 
 exports.init = function(app){
-	app.route('/power').get(function(req,res){
-		console.log(req.param('token'));
+	app.route('/power').get(function(req,res,next){
+
 		if(req.param('token') == config.tokenPower){
 			var script = 'power.sh ' + req.param('id') + ' ' + req.param('status');
 			process.exec(script);
 			res.render('ok',{script : script});	
 
 		}else{
-			console.log(new Date() + 'Fail token');
-			res.status(404).render('404.jade');
+			next();
 		}
 	});
 }

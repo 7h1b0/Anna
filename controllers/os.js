ba510baced3fe7490895587f04a70d5871e1a744
7h1b0 <wyrm1992@hotmail.fr>
2014-12-07 18:11:20 +0100

    Update OS controller

--- controllers/os.js
+++ controllers/os.js
@@ -1,29 +1,17 @@
 var os = require('os')
-	,exec = require('child_process');
+	,exec = require('child_process').exec;
 
 exports.init = function(app){
 
 	app.route('/info').get(function(req,res,next){
-
-		exec('cat /sys/class/thermal/thermal_zone0/temp',function(error, stdout, stderr){
-			    if (error !== null) {
-			    	res.json({
-			    		release: os.release(),
-			    		hostname:os.hostname(),
-			    		plateform:os.platform(),
-			    		uptime:os.uptime(),
-			    		totalmem:os.totalmem(),
-			    		freemem:os.freemem(),
-			    		cpus:os.cpus(),
-			    		tmp : stdout/1000,
-			    		network:os.networkInterfaces()
-			    	});
-					
-			    }else{    	
-			    	res.end(200);
-			    }
-			});
-
-		
+    	res.json({
+    		release: os.release(),
+    		hostname:os.hostname(),
+    		uptime:os.uptime(),
+    		cpus:os.cpus(),
+    		loadavg : os.loadavg(),
+    		totalmem:os.totalmem(),
+    		freemem:os.freemem()			    		
+    	});	
 	});
 }

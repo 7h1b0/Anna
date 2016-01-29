5469b562a6a638cfa6c364c11b7f1bf0e0cd661d
7h1b0 <wyrm1992@laposte.net>
2016-01-29 23:48:22 +0100

    Remove unnecessary method

--- api/helpers/tokenHelper.js
+++ api/helpers/tokenHelper.js
@@ -1,32 +1,12 @@
 const User = require('./../models/user');
 
 module.exports = {
-
 	isValid(token) {
 		return new Promise((resolve, reject) => {
 			User.findOne({token})
 				.select('token')
-				.then(user => {
-					if (!user) {
-						reject();
-					} else {
-						resolve(user);
-					}
-				}).catch(err => reject());
-		});
-	},
-
-	getUserFromToken() {
-		return new Promise((resolve, reject) => {
-			User.findOne({token})
-				.select('token')
-				.then(user => {
-					if (!user) {
-						reject();
-					} else {
-						resolve(user);
-					}
-				}).catch(err => reject());
+				.then(user => !user ? reject() : resolve(user))
+				.catch(err => reject());
 		});
 	}
 }

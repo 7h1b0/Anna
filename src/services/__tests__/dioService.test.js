e9a3822f024ac33a9eddef49d0e81c0b4af6791f
7h1b0 <7h1b0@users.noreply.github.com>
2018-08-04 20:20:10 +0200

    refactor: rewrite dioService

--- src/services/__tests__/dioService.test.js
+++ src/services/__tests__/dioService.test.js
@@ -8,14 +8,16 @@ describe('dioService', () => {
     execService.mockClear();
   });
 
-  it('should call radioEmission script with valid parameters', () => {
-    expect(dioService.add(1, true)).resolves.toBeUndefined();
+  it('should call radioEmission script with valid parameters', async () => {
+    const res = await dioService.add(1, true);
+    expect(res).toBeUndefined();
     expect(execService).toHaveBeenCalledTimes(1);
     expect(execService.mock.calls[0][0]).toEqual(`./radioEmission 1 1`);
   });
 
-  it('should call radioEmission script with valid parameters', () => {
-    expect(dioService.add(1, false)).resolves.toBeUndefined();
+  it('should call radioEmission script with valid parameters', async () => {
+    const res = await dioService.add(1, false);
+    expect(res).toBeUndefined();
     expect(execService).toHaveBeenCalledTimes(1);
     expect(execService.mock.calls[0][0]).toEqual(`./radioEmission 1 0`);
   });
--- src/services/dioService.js
+++ src/services/dioService.js
@@ -1,36 +1,24 @@
 const execService = require('./execService');
 const logger = require('../modules/logger');
 
-const queue = [];
-const run = async () => {
-  if (queue.length !== 0) {
-    const { script, onSuccess, onError } = queue[0];
+let queue = Promise.resolve();
+const run = async (script, onSuccess, onError) => {
+  queue = queue.then(async () => {
     try {
       await execService(script);
       onSuccess();
     } catch (err) {
       onError();
       logger.error(err);
-    } finally {
-      queue.shift();
-      return run();
     }
-  }
+  });
 };
 
 module.exports = {
   add(device, on = false) {
     return new Promise((resolve, reject) => {
       const status = on ? 1 : 0;
-      queue.push({
-        script: `./radioEmission ${device} ${status}`,
-        onSuccess: resolve,
-        onError: reject,
-      });
-
-      if (queue.length === 1) {
-        run();
-      }
+      run(`./radioEmission ${device} ${status}`, resolve, reject);
     });
   },
 };

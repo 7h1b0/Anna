5c0f862a230cebe4f0fdcb208968f92901b821f9
7h1b0 <7h1b0@users.noreply.github.com>
2018-10-04 20:49:33 +0200

    chore(request): use got instead of custom request service

--- src/services/requestService.js
+++ src/services/requestService.js
@@ -1,75 +1,11 @@
-import http from 'http';
-import https from 'https';
-import urlParser from 'url';
+import * as request from 'got';
 
-export function getRoute(url, method = 'GET', body) {
-  const parsed = urlParser.parse(url);
-  const route = {
-    timeout: 2000,
-    headers: { Accept: 'application/json' },
-    method: method.toUpperCase(),
-    protocol: parsed.protocol,
-    hostname: parsed.hostname,
-    path: parsed.path,
-    port: parsed.port,
-  };
-
-  if (body) route.body = body;
-  return route;
+export async function get(url) {
+  const response = await request.get(url, { json: true });
+  return response.body;
 }
 
-export function isSuccess(status) {
-  return status >= 200 && status < 300;
-}
-
-function request(route) {
-  return new Promise((resolve, reject) => {
-    let body;
-    if (route.body) {
-      body = JSON.stringify(route.body);
-      route.headers['content-type'] = 'application/json';
-      route.headers['content-length'] = Buffer.byteLength(body);
-    }
-
-    const protocol = route.protocol === 'https:' ? https : http;
-    const req = protocol.request(route, res => {
-      res.setEncoding('utf8');
-      let rawData = '';
-      res.on('data', chunk => (rawData += chunk));
-      res.on('end', () => {
-        if (isSuccess(res.statusCode)) {
-          try {
-            resolve(JSON.parse(rawData));
-          } catch (e) {
-            reject(e);
-          }
-        } else {
-          reject(rawData);
-        }
-      });
-    });
-    req.on('error', err => reject(err));
-    req.end(body);
-    req.setTimeout(route.timeout, () => reject('timeout'));
-  });
-}
-
-export function get(url) {
-  const route = getRoute(url, 'GET');
-  return request(route);
-}
-
-export function put(url, body) {
-  const route = getRoute(url, 'PUT', body);
-  return request(route);
-}
-
-export function post(url, body) {
-  const route = getRoute(url, 'POST', body);
-  return request(route);
-}
-
-export function del(url) {
-  const route = getRoute(url, 'DELETE');
-  return request(route);
+export async function put(url, body) {
+  const response = await request.post(url, { body, json: true });
+  return response.body;
 }

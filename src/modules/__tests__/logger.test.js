93401bd7fa04437a670da2244e0730844c36145e
7h1b0 <7h1b0@users.noreply.github.com>
2019-01-24 19:35:02 +0100

    chore: remove time from logger and display log on production

--- src/modules/__tests__/logger.test.js
+++ src/modules/__tests__/logger.test.js
@@ -1,15 +1,11 @@
-import lolex from 'lolex';
 import * as logger from '../logger';
 
 describe('Logger', () => {
-  let clock;
   beforeAll(() => {
-    clock = lolex.install({ now: new Date('2018-01-01T10:00') });
     global.console.log = jest.fn();
   });
 
   afterAll(() => {
-    clock.uninstall();
     global.console.log.mockReset();
   });
 
@@ -27,7 +23,7 @@ describe('Logger', () => {
         logger.info('test', true);
 
         expect(console.log).toBeCalled();
-        expect(console.log).toHaveBeenCalledWith('10:00:00 [INFO] test');
+        expect(console.log).toHaveBeenCalledWith('[INFO] test');
       });
     });
 
@@ -36,7 +32,7 @@ describe('Logger', () => {
         logger.warn('test', true);
 
         expect(console.log).toBeCalled();
-        expect(console.log).toHaveBeenCalledWith('10:00:00 [WARN] test');
+        expect(console.log).toHaveBeenCalledWith('[WARN] test');
       });
     });
 
@@ -45,56 +41,7 @@ describe('Logger', () => {
         logger.error('test', true);
 
         expect(console.log).toBeCalled();
-        expect(console.log).toHaveBeenCalledWith('10:00:00 [ERROR] test');
-      });
-    });
-  });
-
-  describe('in production', () => {
-    beforeAll(() => {
-      process.env.NODE_ENV = 'production';
-    });
-
-    afterAll(() => {
-      process.env.NODE_ENV = 'test';
-    });
-
-    describe('info', () => {
-      it('should not be display', () => {
-        logger.info('test');
-
-        expect(console.log).not.toBeCalled();
-      });
-
-      it('should return a valid text is forceDiplay is set', () => {
-        logger.info('test', true);
-
-        expect(console.log).toBeCalled();
-        expect(console.log).toHaveBeenCalledWith('10:00:00 [INFO] test');
-      });
-    });
-
-    describe('warn', () => {
-      it('should not be display', () => {
-        logger.warn('test');
-
-        expect(console.log).not.toBeCalled();
-      });
-
-      it('should return a valid text is forceDiplay is set', () => {
-        logger.warn('test', true);
-
-        expect(console.log).toBeCalled();
-        expect(console.log).toHaveBeenCalledWith('10:00:00 [WARN] test');
-      });
-    });
-
-    describe('error', () => {
-      it('should return a valid text', () => {
-        logger.error('test');
-
-        expect(console.log).toBeCalled();
-        expect(console.log).toHaveBeenCalledWith('10:00:00 [ERROR] test');
+        expect(console.log).toHaveBeenCalledWith('[ERROR] test');
       });
     });
   });
--- src/modules/logger.js
+++ src/modules/logger.js
@@ -1,22 +1,7 @@
-function getTime() {
-  const date = new Date();
-
-  const hours = `0${date.getHours()}`.slice(-2);
-  const minutes = `0${date.getMinutes()}`.slice(-2);
-  const seconds = `0${date.getSeconds()}`.slice(-2);
-  return `${hours}:${minutes}:${seconds}`;
-}
-
 function print(text, level, display) {
   if (process.env.NODE_ENV === 'test') return;
 
-  if (process.env.NODE_ENV === 'production') {
-    if (display || level === 'ERROR') {
-      console.log(`${getTime()} [${level}] ${text}`);
-    }
-  } else {
-    console.log(`${getTime()} [${level}] ${text}`);
-  }
+  console.log(`[${level}] ${text}`);
 }
 
 export function info(text, forceDisplay = false) {

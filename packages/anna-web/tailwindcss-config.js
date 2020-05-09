f1e9dc6bcd7314faa9df2f99baa98051fe452de9
7h1b0 <7h1b0@users.noreply.github.com>
2020-05-09 16:25:02 +0200

    chore: merge typescript, eslint and prettier config (#31)

--- packages/anna-web/tailwindcss-config.js
+++ packages/anna-web/tailwindcss-config.js
@@ -1,11 +1,11 @@
 module.exports = {
+  purge: {
+    content: ['./src/**/*.html', './src/**/*.tsx'],
+  },
   theme: {
     extend: {
       maxWidth: {
         '1440': '1440px',
-      },
-      borderRadius: {
-        xl: '2.5rem',
       },
     },
   },

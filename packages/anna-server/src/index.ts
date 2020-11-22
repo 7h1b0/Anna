5cfee60f19b69eff5ce3ab544ffc5d953756de0b
7h1b0 <7h1b0@users.noreply.github.com>
2020-11-22 19:24:43 +0100

    chore: remove linky integration (#38)

--- packages/anna-server/src/index.ts
+++ packages/anna-server/src/index.ts
@@ -6,7 +6,6 @@ import dio from './modules/dio/route';
 import room from './modules/room/route';
 import hueLight from './modules/hue-light/route';
 import hueSensor from './modules/hue-sensor/route';
-import consumption from './modules/consumption/route';
 import scene from './modules/scene/route';
 import routine from './modules/routine/route';
 import user from './modules/user/route';
@@ -15,7 +14,6 @@ import authentication from './modules/authentication/route';
 import authenticationMiddleware from './modules/authentication/middleware';
 
 import { load as loadRoutine } from './modules/routine/model';
-import { run as runLinky } from './modules/consumption/linkyService';
 const app = express();
 
 app.use(bodyParser.json());
@@ -30,12 +28,10 @@ app.use(authenticationMiddleware, [
   scene,
   routine,
   user,
-  consumption,
 ]);
 
 if (process.env.NODE_ENV !== 'test') {
   loadRoutine();
-  runLinky();
 }
 
 export default app;

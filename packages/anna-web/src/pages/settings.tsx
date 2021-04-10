cc9124b9be6f457f54bab8d8941bb5fe436291ad
7h1b0 <7h1b0@users.noreply.github.com>
2021-04-10 15:17:39 +0200

    chore: update Settings page

--- packages/anna-web/src/pages/settings.tsx
+++ packages/anna-web/src/pages/settings.tsx
@@ -6,33 +6,11 @@ import { ArrowIcon } from 'src/components/icons';
 import Card from 'src/components/card';
 import Typography from 'src/components/typography';
 
-import { useUser } from 'context/user-context';
-
 function Settings() {
-  const user = useUser();
-
   return (
     <>
       <Title title="Settings" />
       <div className="flex gap-2 flex-col">
-        <div className="flex bg-gray-800 rounded p-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            className="fill-current text-teal-500 w-8 mx-auto"
-          >
-            <path d="M5 5a5 5 0 0 1 10 0v2A5 5 0 0 1 5 7V5zM0 16.68A19.9 19.9 0 0 1 10 14c3.64 0 7.06.97 10 2.68V20H0v-3.32z" />
-          </svg>
-          <div className="text-white flex-1 ml-2">
-            <Typography variant="heading" className="capitalize">
-              {user ? user.username : ''}
-            </Typography>
-            <Link to="/logout">
-              <Typography>Logout</Typography>
-            </Link>
-          </div>
-        </div>
-
         <Link to={`/home/rooms/add`}>
           <Card className="justify-between items-center">
             <Typography>Add a room</Typography>
@@ -52,6 +30,12 @@ function Settings() {
             <Typography>Add a hue-light to a room</Typography>
             <ArrowIcon className="fill-current h-4 w-4" />
           </Card>
+        </Link>
+      </div>
+
+      <div className="text-center py-2 my-2">
+        <Link to="/logout">
+          <Typography>Logout</Typography>
         </Link>
       </div>
     </>

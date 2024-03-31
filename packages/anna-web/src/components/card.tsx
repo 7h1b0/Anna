517e7ede065c64b455a6547ec40dbdd477534986
7h1b0 <7h1b0@users.noreply.github.com>
2024-03-31 20:26:27 +0200

    refactor(design): color light background

--- packages/anna-web/src/components/card.tsx
+++ packages/anna-web/src/components/card.tsx
@@ -6,13 +6,15 @@ const Card: React.FC<{
   className?: string;
   tabIndex?: number;
   children: ReactNode;
-}> = ({ children, role, onClick, className, tabIndex }) => {
+  backgroundColor?: string;
+}> = ({ children, role, onClick, className, tabIndex, backgroundColor }) => {
   return (
     <div
       role={role}
       onClick={onClick}
       onKeyPress={onClick}
       tabIndex={tabIndex}
+      style={{ backgroundColor }}
       className={`text-gray-200 shadow-md flex rounded-xl bg-gray-800 py-4 px-2 xl:px-4 hover:bg-gray-700 ${className}`}
     >
       {children}
--- packages/anna-web/src/components/hue-light.tsx
+++ packages/anna-web/src/components/hue-light.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Typography from './typography';
 import Card from './card';
 import type { HueLight as HueLightType } from '@/types/hue-light';
 
@@ -12,50 +11,53 @@ function HueLight({ hueLight }: Props) {
   const { on, hex } = hueLight.state;
   const isColorLight = hueLight.type === 'Extended color light';
 
+  const isLightOn = isColorLight && on;
+
   return (
     <Link to={`/home/rooms/light/${hueLight.id}`}>
-      <Card className="items-center flex-col text-center gap-2 pt-8">
-        {on ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-8 h-8"
-          >
-            <path d="M12 .75a8.25 8.25 0 0 0-4.135 15.39c.686.398 1.115 1.008 1.134 1.623a.75.75 0 0 0 .577.706c.352.083.71.148 1.074.195.323.041.6-.218.6-.544v-4.661a6.714 6.714 0 0 1-.937-.171.75.75 0 1 1 .374-1.453 5.261 5.261 0 0 0 2.626 0 .75.75 0 1 1 .374 1.452 6.712 6.712 0 0 1-.937.172v4.66c0 .327.277.586.6.545.364-.047.722-.112 1.074-.195a.75.75 0 0 0 .577-.706c.02-.615.448-1.225 1.134-1.623A8.25 8.25 0 0 0 12 .75Z" />
-            <path
-              fillRule="evenodd"
-              d="M9.013 19.9a.75.75 0 0 1 .877-.597 11.319 11.319 0 0 0 4.22 0 .75.75 0 1 1 .28 1.473 12.819 12.819 0 0 1-4.78 0 .75.75 0 0 1-.597-.876ZM9.754 22.344a.75.75 0 0 1 .824-.668 13.682 13.682 0 0 0 2.844 0 .75.75 0 1 1 .156 1.492 15.156 15.156 0 0 1-3.156 0 .75.75 0 0 1-.668-.824Z"
-              clipRule="evenodd"
-            />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-8 h-8"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
-            />
-          </svg>
-        )}
-
-        <div className="px-4 flex-1">
-          <Typography>{hueLight.name}</Typography>
-          <Typography variant="caption">{on ? 'On' : 'Off'}</Typography>
+      <Card
+        className="items-center flex-col text-center gap-2 pt-8"
+        backgroundColor={isLightOn ? hex : undefined}
+      >
+        <div className="rounded-full bg-gray-800 p-2">
+          {on ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-8 h-8"
+            >
+              <path d="M12 .75a8.25 8.25 0 0 0-4.135 15.39c.686.398 1.115 1.008 1.134 1.623a.75.75 0 0 0 .577.706c.352.083.71.148 1.074.195.323.041.6-.218.6-.544v-4.661a6.714 6.714 0 0 1-.937-.171.75.75 0 1 1 .374-1.453 5.261 5.261 0 0 0 2.626 0 .75.75 0 1 1 .374 1.452 6.712 6.712 0 0 1-.937.172v4.66c0 .327.277.586.6.545.364-.047.722-.112 1.074-.195a.75.75 0 0 0 .577-.706c.02-.615.448-1.225 1.134-1.623A8.25 8.25 0 0 0 12 .75Z" />
+              <path
+                fillRule="evenodd"
+                d="M9.013 19.9a.75.75 0 0 1 .877-.597 11.319 11.319 0 0 0 4.22 0 .75.75 0 1 1 .28 1.473 12.819 12.819 0 0 1-4.78 0 .75.75 0 0 1-.597-.876ZM9.754 22.344a.75.75 0 0 1 .824-.668 13.682 13.682 0 0 0 2.844 0 .75.75 0 1 1 .156 1.492 15.156 15.156 0 0 1-3.156 0 .75.75 0 0 1-.668-.824Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-8 h-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
+              />
+            </svg>
+          )}
         </div>
-        {isColorLight && (
-          <div
-            className="rounded-full h-8 w-8 bg-black cursor-pointer"
-            style={{ backgroundColor: hex }}
-          />
-        )}
+
+        <div
+          className={`px-4 flex-1 ${isLightOn ? 'text-black' : 'text-white'}`}
+        >
+          <p className="text-3x">{hueLight.name}</p>
+          <p className="text-xs">{on ? 'On' : 'Off'}</p>
+        </div>
       </Card>
     </Link>
   );
